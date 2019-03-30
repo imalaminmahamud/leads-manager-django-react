a1286749e668c3a69e9d2731103f2a80c4a54a79
Md. Alamin Mahamud <alamin.ineedahelp@gmail.com>
2019-03-30 18:47:12 +0600

    Dataprovider

---
+++ frontend/src/components/DataProvider.js
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+
+class DataProvider extends Component {
+  static propTypes = {
+    endpoint: PropTypes.string.isRequired,
+    render: PropTypes.func.isRequired
+  };
+
+  state = {
+    data: [],
+    loaded: false,
+    placeholder: "Loading..."
+  };
+
+  componentDidMount() {
+    console.log(this.props.endpoint);
+    fetch(this.props.endpoint)
+      .then(response => {
+        if (response.status !== 200) {
+          return this.setState({
+            placeholder: "Something went wrong!"
+          });
+        }
+
+        return response.json();
+      })
+      .then(data =>
+        this.setState({
+          data,
+          loaded: true
+        })
+      );
+  }
+
+  render() {
+    const { data, loaded, placeholder } = this.state;
+    return loaded ? this.props.render(data) : <p>Placeholder</p>;
+  }
+}
+
+export default DataProvider;
