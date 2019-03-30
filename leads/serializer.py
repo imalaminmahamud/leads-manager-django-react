70f176fcb182f4c5ce512b430183f523aea5c009
Md. Alamin Mahamud <alamin.ineedahelp@gmail.com>
2019-03-30 18:44:24 +0600

    serializer

---
+++ leads/serializer.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+from .models import Lead
+
+
+class LeadSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Lead
+        fields = (
+            'id',
+            'name',
+            'email',
+            'message',
+        )
