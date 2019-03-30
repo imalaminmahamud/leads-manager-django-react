60ac0c14bcb868da040894c8c0b39695ef4d1cdf
Md. Alamin Mahamud <alamin.ineedahelp@gmail.com>
2019-03-30 18:46:57 +0600

    App

---
+++ frontend/src/components/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+import DataProvider from "./DataProvider";
+import Table from "./Table";
+
+const App = () => (
+  <DataProvider endpoint="/api/lead/" render={data => <Table data={data} />} />
+);
+
+export default App;
